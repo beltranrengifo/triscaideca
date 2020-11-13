15a60be7ec507ca5dd708430005c1a6b95fb65ef
beltranrengifo <beltran@rengifo.es>
2020-11-13 11:24:04 +0100

    fix: tailwind lint rules

---
+++ components/common/header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header"><p>header</p></header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Header',
+})
+</script>
+
+<style scoped lang="scss"></style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      sans: ['Oswald', 'Arial', 'sans-serif'],
+    },
+    fontWeight: {
+      normal: 300,
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
