1742f776219acfb2002f3debe2bf5eb1babad7ee
beltranrengifo <beltran@rengifo.es>
2020-11-16 18:05:42 +0100

    feat: add all content

---
+++ pages/projects/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <article>
+    <pre> {{ project }} </pre>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Project',
+
+  async asyncData({ $content, params }) {
+    const project = await $content('projects', params.slug).fetch()
+
+    return { project }
+  },
+})
+</script>
