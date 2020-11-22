433c94248c0d7de0dee14fb56016cb3763ade566
beltranrengifo <beltran@rengifo.es>
2020-11-22 13:42:43 +0100

    feat: favicon. feat: head metas

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,15 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'triscaideca',
+    title: 'Triscaideca - Arquitectura de interiores',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Arquitectura de interiores y proyectos de interiorismo',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
