1bab1cf95ae810a776318dcc9ac34fa9ae2c9524
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-02-26 20:34:14 +0100

    footer improvements

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,24 +1,14 @@
 <template>
   <container tag="footer" class="main-footer">
     <div>
-      <Logo small />
-      <p class="mt-1">
+      <Logo />
+      <p class="mt-1 pl-4">
         <small class="text-xs">
           {{ copy }}
         </small>
       </p>
     </div>
-    <ul class="footer__nav">
-      <li
-        v-for="(value, key) in $t('navigation')"
-        :key="key"
-        class="footer__nav-item mb-2"
-      >
-        <n-link :to="`/${key === 'projects' ? '' : key}`" class="uppercase">
-          {{ value }}
-        </n-link>
-      </li>
-    </ul>
+    <Navigation />
   </container>
 </template>
 
@@ -38,23 +28,9 @@ export default Vue.extend({
 
 <style lang="scss">
 .main-footer {
-  padding: rem(164) rem(16) rem(64);
+  padding: rem(64) rem(32);
   position: relative;
   display: flex;
   justify-content: space-between;
-
-  &:before {
-    content: '';
-    height: 1px;
-    background-color: var(--color-base);
-    position: absolute;
-    width: 60%;
-    left: 20%;
-    top: 74px;
-  }
-
-  .nuxt-link-exact-active {
-    border-bottom: 1px solid;
-  }
 }
 </style>
